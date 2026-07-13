dfa9a8be3b0ae838a0cb32414de13f7abacb7e8d
samocology <godstimesml@gmail.com>
2026-07-13 10:21:35 +0100

    added

---
+++ api/index.js
@@ -0,0 +1,9 @@
+﻿import { createServer } from 'http'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+export default async function handler(req, res) {
+  const html = readFileSync(join(process.cwd(), 'dist/client/index.html'), 'utf-8')
+  res.setHeader('Content-Type', 'text/html')
+  res.send(html)
+}
